633ddca54472ba8ebb26a1296ac64145fbe16697
codexmish <majharulislam.shanto.70@gmail.com>
2025-10-21 09:08:26 +0600

    navbar done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -34,19 +34,25 @@ const Navbar = () => {
 
           {/* -------menu---------- */}
           <div className="flex items-center gap-15">
-
-            {
-            menuitem.map((item, id)=>(
-              <a
+            {menuitem.map((item, id) => (
+              <Link
                 className="text-sm text-white font-medium font-playfair"
                 key={id}
                 to={item.navLink}
               >
                 {item.menuName}
-              </a>
-            ))
-            }
+              </Link>
+            ))}
+          </div>
 
+          {/* --------reg and login button---- */}
+          <div className="buttons flex items-center gap-9">
+            <div className="sin-button w-[100px] h-[45px] flex items-center justify-center bg-transparent text-sm text-text-black font-medium font-playfair rounded-full">
+              Sing In
+            </div>
+            <div className="sin-button w-[100px] h-[45px] flex items-center justify-center bg-theme text-sm text-white font-medium font-playfair rounded-full">
+              Sing Up
+            </div>
           </div>
         </div>
       </nav>
