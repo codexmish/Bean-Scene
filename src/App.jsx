30d0ef46fa6af28c422549f9133897b34c240798
codexmish <majharulislam.shanto.70@gmail.com>
2025-10-21 09:01:17 +0600

    router setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import './App.css'
 import Home from './pages/Home'
+import { createBrowserRouter, createRoutesFromElements, Route, Router, RouterProvider } from 'react-router'
+import LayoutOne from './layout/LayoutOne'
 const App = () => {
+  const myRoute = createBrowserRouter(createRoutesFromElements(
+    <Route path='/' element={<LayoutOne/>}>
+      <Route index element={<Home/>}/>
+  
+
+    </Route>
+  ))
   return (
+
+
+
     <>
-    <Home/>
+    <RouterProvider router={myRoute}/>
     
     </>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,57 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router";
 
 const Navbar = () => {
+  const menuitem = [
+    {
+      menuName: "Home",
+      navLink: "/",
+    },
+    {
+      menuName: "Menu",
+      navLink: "/",
+    },
+    {
+      menuName: "About Us",
+      navLink: "/",
+    },
+    {
+      menuName: "contact Us",
+      navLink: "/",
+    },
+  ];
+
   return (
     <>
-    
-    </>
-  )
-}
+      <nav className="py-5 bg-gray-500">
+        <div className="main container flex items-center justify-between">
+          {/* --------logo------- */}
+          <div className="logo">
+            <h1 className="text-4xl text-white font-normal font-clicker">
+              Bean Scene
+            </h1>
+          </div>
 
-export default Navbar
+          {/* -------menu---------- */}
+          <div className="flex items-center gap-15">
+
+            {
+            menuitem.map((item, id)=>(
+              <a
+                className="text-sm text-white font-medium font-playfair"
+                key={id}
+                to={item.navLink}
+              >
+                {item.menuName}
+              </a>
+            ))
+            }
+
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
