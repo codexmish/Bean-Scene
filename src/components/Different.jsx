35abc09d386344217edfe1d3c001200d60bfabcb
codexmish <majharulislam.shanto.70@gmail.com>
2025-10-22 19:39:03 +0600

    chance section added with res

--- src/components/Different.jsx
+++ src/components/Different.jsx
@@ -5,12 +5,16 @@ import { PiCoffeeBean } from "react-icons/pi";
 import { LiaCertificateSolid } from "react-icons/lia";
 import { BsCupHot } from "react-icons/bs";
 import { MdOutlinePriceCheck } from "react-icons/md";
+import blast from "../assets/images/coffee_blast_right.png";
 
 const Different = () => {
   return (
     <>
-      <section>
-        <div className="main mt-10 lg:mt-20 container px-6 lg:px-o">
+      <section className="relative w-full pb-21">
+        <div className="absolute right-0 top-[-200px]">
+          <img src={blast} alt="" />
+        </div>
+        <div className="main mt-15 lg:mt-20 container px-6 lg:px-o">
           <CommonHead
             head={"Why are we different?"}
             pp={"We don't just make your coffee, we make your day!"}
@@ -42,20 +46,17 @@ const Different = () => {
             />
           </div>
 
-
           <div className="flex flex-col items-center">
-
-          <CommonHead
-            pp={
-              "Great ideas start with great coffee, Lets help you achieve that"
-            }
-          />
-          <CommonHead head={"Get started today."} />
-          <div className="mt-6 lg:mt-10 sin-button cursor-pointer w-[100px] h-[45px] flex items-center justify-center bg-theme text-sm text-text-black hover:text-white font-medium font-playfair rounded-full">
+            <CommonHead
+              pp={
+                "Great ideas start with great coffee, Lets help you achieve that"
+              }
+            />
+            <CommonHead head={"Get started today."} />
+            <div className="mt-6 lg:mt-10 sin-button cursor-pointer w-[100px] h-[45px] flex items-center justify-center bg-theme text-sm text-text-black hover:text-white font-medium font-playfair rounded-full">
               Join Us
             </div>
           </div>
-
         </div>
       </section>
     </>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Banner from "../components/Banner";
 import Discover from "../components/Discover";
 import Different from "../components/Different";
 import Enjoy from "../components/Enjoy";
+import Chance from "../components/Chance";
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <Discover/>
       <Enjoy/>
       <Different/>
+      <Chance/>
     </>
   );
 };
